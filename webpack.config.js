91b0ddffdfeef50615e99cc635dd05d7ae270703
Terry O'Shea <terrykoshea@gmail.com>
2019-02-22 16:38:52 -0500

    add js minification; fix serving bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+        minimizer: [new UglifyJsPlugin()]
     },
     module: {
         rules: [
